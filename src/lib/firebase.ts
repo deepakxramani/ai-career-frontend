8de6ee7a09ef72e904a17e95e077e81fad45dafd
Deepak <deepakramani65@gmail.com>
2026-07-09 21:11:50 +0530

    removed consoles

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,8 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
-console.log("Firebase:", process.env.NEXT_PUBLIC_API_KEY);
-
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
